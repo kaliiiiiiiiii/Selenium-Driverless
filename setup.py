0ee2871d1d0550506ee863dddca2d12add82517a
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-07-21 16:43:10 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-requirements = ['selenium~=4.6', 'aiohttp==3.6.2', 'websockets==7.0',
-                'beautifulsoup4==4.7.1', 'lxml==4.6.2', 'pyyaml==5.1',
+requirements = ['selenium~=4.6', 'aiohttp==3.8.5', 'websockets==11.0.3',
+                'beautifulsoup4==4.7.1', 'lxml==4.6.2', 'pyyaml==6.0.1',
                 'Pillow==7.1.0']
 
 with open('README.md', 'r', encoding='utf-8') as fh:
