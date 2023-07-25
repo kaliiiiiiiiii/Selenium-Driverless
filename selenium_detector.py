3eb9de74358dab30c683eee1210a662994e2d872
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-07-26 00:50:56 +0200

    updated selenium_detector.py

--- selenium_detector.py
+++ selenium_detector.py
@@ -1,4 +1,5 @@
 from selenium_driverless import webdriver
+from selenium_driverless.types.by import By
 import asyncio
 
 
@@ -6,12 +7,12 @@ async def main():
     options = webdriver.Options()
     async with webdriver.Chrome(options=options) as driver:
         await driver.get('https://hmaker.github.io/selenium-detector/')
-        elem = await driver.execute_script('return document.querySelector("#chromedriver-token")')
+        elem = await driver.find_element(By.CSS_SELECTOR, "#chromedriver-token")
         await elem.send_keys(await driver.execute_script('return window.token'))
-        elem2 = await driver.execute_script('return document.querySelector("#chromedriver-asynctoken")')
+        elem2 = await driver.find_element(By.CSS_SELECTOR, "#chromedriver-asynctoken")
         async_token = await driver.execute_async_script('window.getAsyncToken().then(arguments[0])')
         await elem2.send_keys(async_token)
-        elem3 = await driver.execute_script('return document.querySelector("#chromedriver-test")')
+        elem3 = await driver.find_element(By.CSS_SELECTOR, "#chromedriver-test")
         await elem3.click()
         print(await driver.title)
 
