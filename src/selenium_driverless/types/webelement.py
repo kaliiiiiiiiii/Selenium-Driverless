553ea8e970f63a29c5b621cdbd86cdcf6dec86a5
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-08-07 23:11:29 +0200

    patch elem.click() function for undetectability

--- src/selenium_driverless/types/webelement.py
+++ src/selenium_driverless/types/webelement.py
@@ -158,7 +158,22 @@ class WebElement(RemoteObject):
 
     async def click(self) -> None:
         """Clicks the element."""
-        await self.execute_script("this.click()")
+        script = """
+        // https://stackoverflow.com/a/38073679/20443541
+        function fireClick(node){
+            if (document.createEvent) {
+                var evt = document.createEvent('MouseEvents');
+                evt.initEvent('click', true, false);
+                node.dispatchEvent(evt);    
+            } else if (document.createEventObject) {
+                node.fireEvent('onclick') ; 
+            } else if (typeof node.onclick == 'function') {
+                node.onclick(); 
+            }
+        };
+        fireClick(this)
+        """
+        await self.execute_script(script=script)
 
     async def submit(self):
         """Submits a form."""
