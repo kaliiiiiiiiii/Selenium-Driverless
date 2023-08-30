0fb4531c38aa51b1490646d2cd40ce2023eece85
kaliiiiiiiiii <aurinliun@gmx.ch>
2023-08-30 13:32:57 +0200

    ensure `driver.search_elements` always works (fix bugs)

--- src/selenium_driverless/webdriver.py
+++ src/selenium_driverless/webdriver.py
@@ -87,6 +87,7 @@ class Chrome(BaseWebDriver):
         if disconnect_connect:
             warnings.warn("disconnect_connect=True might be buggy")
         self._page_enabled = None
+        self._dom_enabled = None
 
         self._global_this_ = None
         self._document_elem_ = None
@@ -206,7 +207,7 @@ class Chrome(BaseWebDriver):
 
         # noinspection PyProtectedMember
         self._is_remote = self._options._is_remote
-        
+
         self._pointer = Pointer(driver=self)
 
         if not self._is_remote:
@@ -362,7 +363,7 @@ class Chrome(BaseWebDriver):
         if not self._global_this_:
             self._global_this_ = await RemoteObject(driver=self, js="globalThis", check_existence=False)
         return self._global_this_
-    
+
     @property
     def pointer(self) -> Pointer:
         return self._pointer
@@ -805,14 +806,23 @@ class Chrome(BaseWebDriver):
         """
         query:str | Plain text or query selector or XPath search query.
         """
+        # ensure DOM is enabled
+        if not self._dom_enabled:
+            await self.execute_cdp_cmd("DOM.enable")
+
+        # ensure DOM.getDocument got called
+        await self._document_elem
+
         elems = []
         res = await self.execute_cdp_cmd("DOM.performSearch",
                                          {"includeUserAgentShadowDOM": True, "query": query})
         search_id = res["searchId"]
         elem_count = res["resultCount"]
+        if elem_count <= 0:
+            return []
 
         res = await self.execute_cdp_cmd("DOM.getSearchResults",
-                                         {"searchId": search_id, "fromIndex": 0, "toIndex": elem_count - 1})
+                                         {"searchId": search_id, "fromIndex": 0, "toIndex": elem_count})
         for node_id in res["nodeIds"]:
             if self._loop:
                 elem = await SyncWebElement(driver=self, check_existence=False, node_id=node_id, loop=self._loop)
@@ -1320,6 +1330,12 @@ class Chrome(BaseWebDriver):
             self._page_enabled = True
         elif cmd == "Page.disable":
             self._page_enabled = False
+
+        elif cmd == "DOM.enable":
+            self._dom_enabled = True
+        elif cmd == "DOM.disable":
+            self._dom_enabled = False
+
         if disconnect_connect:
             await socket.close()
             self._page_enabled = False
