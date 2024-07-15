7232423ebdac1d8aabce44aeb6d1946703773ec1
kaliiiiiiiiii <aurinliun@gmx.ch>
2024-07-15 19:47:45 +0200

    add further tests for mouse

--- tests/interaction/test_mouse.py
+++ tests/interaction/test_mouse.py
@@ -1,3 +1,5 @@
+import typing
+
 import pytest
 import time
 from cdp_patches.input import AsyncInput
@@ -10,42 +12,30 @@ script = """
     window.events = []
     b = document.body
     function jsonify(e){
+        if (e !== Object(e)){
+            // is primitive value
+            return e
+        }
         var res = {}
         var val
         for (key in e){
-            val = e[key]
-            if (key === "timeStamp" && typeof val === "number"){
-                // normalize timestamp to UTC seconds
-                val = (val + performance.timeOrigin)/1000
-            }else if (key === "sourceCapabilities"){
-                var _val = jsonify(val)
-                val = _val
-                key = "sourceCapabilities"
-            }
-            res[key]=val
+            res[key]=e[key]
         }
         return res
     }
     
     function h(e){
-        window.events.push(jsonify(e))
+        var res = jsonify(e)
+        res["sourceCapabilities"] = jsonify(e.sourceCapabilities)
+        window.events.push(res)
     }
     b.addEventListener("click", h)
+    b.addEventListener("mousedown", h)
+    b.addEventListener("mouseup", h)
     """
 
-click_expected = {
+mouse_event_expected = {
     "isTrusted": True,
-    "pointerId": 1,
-    "width": 1,
-    "height": 1,
-    "pressure": 0,
-    "tiltX": 0,
-    "tiltY": 0,
-    "azimuthAngle": 0,
-    "tangentialPressure": 0,
-    "twist": 0,
-    "pointerType": "mouse",
-    "isPrimary": False,
     "ctrlKey": False,
     "shiftKey": False,
     "altKey": False,
@@ -56,10 +46,8 @@ click_expected = {
     "movementX": 0,
     "movementY": 0,
     "fromElement": None,
-    "toElement": None,
     "detail": 1,
     "which": 1,
-    "type": "click",
     "eventPhase": 2,
     "bubbles": True,
     "cancelable": True,
@@ -72,89 +60,141 @@ click_expected = {
     "AT_TARGET": 2,
     "BUBBLING_PHASE": 3
 }
+mouse_expected_keys = {"screenX", "screenY", "clientX", "clientY",
+                       "pageX", "pageY", "x", "y", 'offsetX', 'offsetY', "layerX", "layerY",
+                       'getModifierState', 'initMouseEvent', 'initUIEvent', 'currentTarget',
+                       'stopPropagation', 'timeStamp', "sourceCapabilities", 'srcElement',
+                       'composedPath', 'initEvent', 'preventDefault', 'stopImmediatePropagation',
+                       "view", "target", "type", "toElement"}
+
+click_expected = {
+    "azimuthAngle": 0,
+    "height": 1,
+    "tiltX": 0,
+    "tiltY": 0,
+    "pointerType": "mouse",
+    "tangentialPressure": 0,
+    "twist": 0,
+    "isPrimary": False,
+    "pointerId": 1,
+    "width": 1,
+    "pressure": 0,
+}
+click_expected_keys = {
+    'getPredictedEvents',
+    'getCoalescedEvents',
+    "altitudeAngle"
+}
+mousedown_expected = {
+    "buttons": 1,
+}
 
 
 def in_range(num, expected, _range):
     return (expected + _range) >= num >= (expected - _range)
 
 
-def validate_click_event(event: dict, x, y, timestamp_range=2, pixel_range=1):
+def validate_mouse_event(subtests, event: dict, x, y, time_origin: float,
+                         _type: typing.Literal["mousedown", "mouseup", "click"],
+                         timestamp_range=2, pixel_range=1):
     now = time.time()
-    expected_keys = {'getCoalescedEvents', 'getPredictedEvents', "screenX", "screenY", "clientX", "clientY",
-                     "pageX", "pageY", "x", "y", 'offsetX', 'offsetY', "layerX", "layerY", 'getModifierState',
-                     'initMouseEvent', 'initUIEvent', 'currentTarget', 'stopPropagation', 'timeStamp',
-                     "sourceCapabilities",
-                     'srcElement', 'composedPath', 'initEvent', 'preventDefault', 'stopImmediatePropagation', "view",
-                     "target", "altitudeAngle"}
-    for key in click_expected.keys():
-        expected_keys.add(key)
+
+    expected = mouse_event_expected.copy()
+    expected.update(mouse_event_expected)
+    if _type == "click":
+        expected.update(click_expected)
+    elif _type == "mousedown":
+        expected.update(mousedown_expected)
+
+    expected_keys = mouse_expected_keys.copy()
+    expected_keys.update(expected.keys())
+    if _type == "click":
+        expected_keys.update(click_expected_keys)
 
     for key, value in event.items():
-        expected_value = click_expected.get(key, special)
+        expected_value = expected.get(key, special)
         if expected_value == special:
-            if key in expected_keys:
-                expected_keys.remove(key)
-            if key in ['getCoalescedEvents', 'getPredictedEvents', 'getModifierState', 'initMouseEvent', 'initUIEvent',
-                       'stopPropagation', 'composedPath', 'initEvent', 'preventDefault', 'stopImmediatePropagation']:
-                assert isinstance(value, JSFunction)
-            elif key == "screenX":
-                assert not in_range(value, x, pixel_range)
-            elif key == "screenY":
-                assert not in_range(value, y, pixel_range)
-            elif key in ["clientX", "x"]:
-                assert in_range(value, x, pixel_range)
-            elif key in ["clientY", "y"]:
-                assert in_range(value, y, pixel_range)
-            elif key == "pageX":
-                pass
-                # todo: assert
-                # assert value == x
-            elif key == "pageY":
-                pass
-                # todo: assert
-                # assert value == y
-            elif key == 'offsetX':
-                pass
-                # todo: assert
-                # assert value == x
-            elif key == 'offsetY':
-                pass
-                # todo: assert
-                # assert value == y
-            elif key == "layerX":
-                pass
-                # todo:
-                # assert value == x
-            elif key == "layerY":
-                pass
-                # todo:
-                # assert value == y
-            elif key in ['currentTarget', 'srcElement', "target"]:
-                assert (isinstance(value, WebElement) or value is None)
-            elif key == 'timeStamp':
-                # maximum timestamp_range ago
-                assert value <= now
-                assert value >= (now - timestamp_range)
-            elif key == "sourceCapabilities":
-                assert len(value) == 1
-                assert value["firesTouchEvents"] is False
-            elif key == "view":
-                assert (isinstance(value, JSWindow) or value is None)
-            elif key == "altitudeAngle":
-                # 90° in rad expected
-                assert in_range(value, 1.5708, 0.0001)
-            else:
-                raise ValueError(f"Got unknown, unexpected attribute for event\n {key.__repr__()}={value.__repr__()}")
+            # no constant == assertion
+            with subtests.test():
+                if key in expected_keys:
+                    expected_keys.remove(key)
+                if key in ['getCoalescedEvents', 'getPredictedEvents', 'getModifierState', 'initMouseEvent',
+                           'initUIEvent',
+                           'stopPropagation', 'composedPath', 'initEvent', 'preventDefault',
+                           'stopImmediatePropagation']:
+                    assert isinstance(value, JSFunction)
+                elif key == "screenX":
+                    assert not in_range(value, x, pixel_range)
+                elif key == "screenY":
+                    assert not in_range(value, y, pixel_range)
+                elif key in ["clientX", "x"]:
+                    assert in_range(value, x, pixel_range)
+                elif key in ["clientY", "y"]:
+                    assert in_range(value, y, pixel_range)
+                elif key == "pageX":
+                    pass
+                    # todo: assert
+                    # assert value == x
+                elif key == "pageY":
+                    pass
+                    # todo: assert
+                    # assert value == y
+                elif key == 'offsetX':
+                    pass
+                    # todo: assert
+                    # assert value == x
+                elif key == 'offsetY':
+                    pass
+                    # todo: assert
+                    # assert value == y
+                elif key == "layerX":
+                    pass
+                    # todo:
+                    # assert value == x
+                elif key == "layerY":
+                    pass
+                    # todo:
+                    # assert value == y
+                elif key in ['currentTarget', 'srcElement', "target"]:
+                    assert (isinstance(value, WebElement) or value is None)
+                elif key == 'timeStamp':
+
+                    # timeStamp to UTC in seconds
+                    value = (time_origin + value) / 1000
+
+                    # maximum timestamp_range ago
+                    assert value <= now
+                    assert value >= (now - timestamp_range)
+                elif key == "sourceCapabilities":
+                    assert len(value) == 1
+                    assert value["firesTouchEvents"] is False
+                elif key == "view":
+                    assert (isinstance(value, JSWindow) or value is None)
+                elif key == "altitudeAngle":
+                    # 90° in rad expected
+                    assert in_range(value, 1.5708, 0.0001)
+                elif key == "type":
+                    assert value == _type
+                elif key == "toElement":
+                    if _type == "click":
+                        assert value is None
+                    else:
+                        assert isinstance(value, WebElement)
+                else:
+                    raise ValueError(
+                        f"Got unknown, unexpected attribute for event\n {key.__repr__()}={value.__repr__()}")
         else:
             expected_keys.remove(key)
-            if not (value == expected_value):
-                print()
-            assert value == expected_value
-    assert len(expected_keys) == 0
+
+            with subtests.test(key=key):
+                assert value == expected_value
+    with subtests.test():
+        assert len(expected_keys) == 0
 
 
-async def click_tester(driver, cdp_patches=False):
+async def click_tester(subtests, driver, cdp_patches=False):
     await driver.current_target.execute_script(script)
+    event_types = ["mousedown", "mouseup", "click"]
     cords = [
         [100, 50],
         [200, 100],
@@ -170,24 +210,27 @@ async def click_tester(driver, cdp_patches=False):
         for x, y in cords:
             await p.click(x, y, move_to=False)
 
-    events = await driver.execute_script("return window.events", max_depth=10)
-    assert len(events) == 3
-    for idx, event in enumerate(events):
+    events, time_origin = await driver.execute_script("return [window.events, performance.timeOrigin]", max_depth=10)
+    assert len(events) == len(cords) * 3
+
+    for idx in range(len(cords)):
         x, y = cords[idx]
-        validate_click_event(event, x, y)
+        grouped_events = events[idx * 3:(idx * 3) + 3]
+        for _type, event in zip(event_types, grouped_events):
+            validate_mouse_event(subtests, event, x, y, time_origin, _type)
 
 
 @pytest.mark.asyncio
-async def test_click(driver):
-    await click_tester(driver)
+async def test_click(driver, subtests):
+    await click_tester(subtests, driver)
 
 
 @pytest.mark.skip(reason="Headless will always fail")
 @pytest.mark.asyncio
-async def test_headless_click(h_driver):
-    await click_tester(h_driver)
+async def test_headless_click(h_driver, subtests):
+    await click_tester(subtests, h_driver)
 
 
 @pytest.mark.asyncio
-async def test_click_with_cdp_patches(driver):
-    await click_tester(driver, cdp_patches=True)
+async def test_click_with_cdp_patches(driver, subtests):
+    await click_tester(subtests, driver, cdp_patches=True)
