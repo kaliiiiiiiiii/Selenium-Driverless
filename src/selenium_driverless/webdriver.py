185bda6a48e06ecb1acdff4aaa03812c7319fe60
Yacine S. <38400007+Redrrx@users.noreply.github.com>
2024-07-23 16:30:58 +0100

    exposed port

--- src/selenium_driverless/webdriver.py
+++ src/selenium_driverless/webdriver.py
@@ -120,6 +120,8 @@ class Chrome:
         self._is_remote = False
         self._has_incognito_contexts: bool = False
         self._started = False
+        self.port: int or None = None
+
 
     def __repr__(self):
         return f'<{type(self).__module__}.{type(self).__name__} (session="{self.current_target.id}")>'
@@ -299,6 +301,8 @@ class Chrome:
                     context._closed_callbacks.append(remove_context)
                     self._current_context = context
                     self._base_context = context
+                    self.port = self.base_context.base_target.socket.ws_url.split(":")[2].split("/")[0]
+
                     self._contexts[_id] = context
                     break
             await self.execute_cdp_cmd("Emulation.setFocusEmulationEnabled", {"enabled": True})
